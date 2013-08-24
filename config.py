51f4964867ad65cc1ebd6006025c141f774e4455
dccrazyboy <dccrazyboy@gmail.com>
2013-08-24 22:09:43 +0800

    add comment

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 #coding:utf8
 
+# where to store the music file (master cdn)
 MUSIC_FILE_PATH = '/home/dc/Music/music_file/'
 
-ITEM_PER_PAGE = 5
+# how many items in one page (for admin pages)
+ITEM_PER_PAGE = 10
