81af97e8e7c8d43dbd7a91879ca90d9c2868b5c4
fz1989 <fz1989fz@gmail.com>
2013-10-25 21:53:28 +0800

    rename user to user_id

--- Master/user/recommendations.py
+++ Master/user/recommendations.py
@@ -8,44 +8,45 @@ def get_prefs():
     all_user = UserSet.get_all_user()
     for user in all_user:
         for music_id in user.user_listened:
-            ret.setdefault(user, {})
-            ret[user][music_id] = 0
+            ret.setdefault(user.user_id, {})
+            ret[user.user_id][music_id] = 0
         for music_id in user.user_favour:
-            ret.setdefault(user, {})
-            ret[user][music_id] = 1
+            ret.setdefault(user.user_id, {})
+            ret[user.user_id][music_id] = 1
         for music_id in user.user_dislike:
-            ret[user][music_id] = -1
+            ret[user.user_id][music_id] = -1
 
     return ret
 
-def sim_distance(prefs, user1, user2):
+def sim_distance(prefs, user1_id, user2_id):
     si = {}
-    for item in prefs[user1]:
-        if item in prefs[user2]:
+    for item in prefs[user1_id]:
+        if item in prefs[user2_id]:
             si[item] = 1
     if len(si) == 0:
         return 0
-    sum_of_squares = sum([ pow(prefs[user1][item] - prefs[user2][item], 2) for item in prefs[user1] if item in prefs[user2]])
+    sum_of_squares = sum([ pow(prefs[user1_id][item] - prefs[user2_id][item], 2) for item in
+        prefs[user1_id] if item in prefs[user2_id]])
     return 1 / (1 + sqrt(sum_of_squares))
 
-def top_k_matches(prefs, user, k = 10, similarity = sim_distance):
-    scores = [(similarity(prefs, user, other), other) for other in prefs if other != user]
+def top_k_matches(prefs, user_id, k = 10, similarity = sim_distance):
+    scores = [(similarity(prefs, user_id, other), other) for other in prefs if other != user_id]
     scores.sort()
     scores.reverse()
     return scores[0:k]
 
-def get_recommendations_with_user_based(prefs, user, similarity = sim_distance):
+def get_recommendations_with_user_based(prefs, user_id, similarity = sim_distance):
     totals = {}
     simSums = {}
     for other in prefs:
-        if other == user:
+        if other == user_id:
             continue
-        sim = similarity(prefs, user, other)
+        sim = similarity(prefs, user_id, other)
 
         if sim <= 0:
             continue
         for item in prefs[other]:
-            if item not in prefs[user] or prefs[user] == 0:
+            if item not in prefs[user_id] or prefs[user_id] == 0:
                 totals.setdefault(item, 0)
                 totals[item] += prefs[other] * sim
                 simSums[item] += sim
@@ -57,11 +58,11 @@ def get_recommendations_with_user_based(prefs, user, similarity = sim_distance):
 
 def transform_prefs(prefs):
     ret = {}
-    for user in prefs:
-        for item in prefs[user]:
+    for user_id in prefs:
+        for item in prefs[user_id]:
             ret.setdefault(item, {})
 
-            ret[item][user] = prefs[user][item]
+            ret[item][user_id] = prefs[user_id][item]
 
     return ret
 
@@ -73,8 +74,8 @@ def calc_item_similarity_items(prefs, k = 10):
         ret[item] = scores
     return ret
 
-def get_recommendations_with_item_based(prefs, user):
-    user_rating = prefs[user]
+def get_recommendations_with_item_based(prefs, user_id):
+    user_rating = prefs[user_id]
     scores = {}
     total_sim = {}
     item_mat = calc_item_similarity_items(prefs)
@@ -92,8 +93,8 @@ def get_recommendations_with_item_based(prefs, user):
     rankings.reverse()
     return rankings
 
-def get_next_musics(user, recommend_algo = get_recommendations_with_item_based):
+def get_next_musics(user_id, recommend_algo = get_recommendations_with_item_based):
     prefs = get_prefs()
-    ret = [music_id for (score, music_id) in recommend_algo(prefs, user)]
+    ret = [music_id for (score, music_id) in recommend_algo(prefs, user_id)]
     return ret
 
